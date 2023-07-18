249f8896f5d8bcc64faad21202d8da7e6116a23b
sumit <118124900+sumit-yu-bot@users.noreply.github.com>
2023-07-18 22:45:52 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -90,78 +90,75 @@ async def gen_thumb(videoid, user_id):
         e = np.dstack((c, d))
         f = Image.fromarray(e)
         x = f.resize((107, 107))
+      
+  youtube = Image.open(f"cache/thumb{videoid}.jpg")
+  image1 = changeImageSize(1280, 720, youtube)
+  image2 = image1.convert("RGBA")
+  background = image2.filter(filter=ImageFilter.BoxBlur(30))enhancer = ImageEnhance.Brightness(background)
+  background = enhancer.enhance(0.6)
+  image2 = background
 
-            youtube = Image.open(f"cache/thumb{videoid}.jpg")
-            image1 = changeImageSize(1280, 720, youtube)
-            image2 = image1.convert("RGBA")
-            background = image2.filter(filter=ImageFilter.BoxBlur(30))
-            enhancer = ImageEnhance.Brightness(background)
-            background = enhancer.enhance(0.6)
-            image2 = background
+circle = Image.open("AnonX/assets/sumit.png")
 
-            circle = Image.open("AnonX/assets/sumit.png")
-            
-            im = circle
-            im = im.convert('RGBA')
-            color = make_col()
+im = circle
+im = im.convert('RGBA')
+color = make_col()
 
-            data = np.array(im)
-            red, green, blue, alpha = data.T
+data = np.array(im)
+red, green, blue, alpha = data.T
+white_areas = (red == 255) & (blue == 255) & (green == 255)
+data[..., :-1][white_areas.T] = color
+im2 = Image.fromarray(data)
+circle = im2
+image3 = image1.crop((280,0,1000,720))
+lum_img = Image.new('L', [720,720] , 0)
+draw = ImageDraw.Draw(lum_img)
+draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
+img_arr = np.array(image3)
+lum_img_arr = np.array(lum_img)
+final_img_arr = np.dstack((img_arr,lum_img_arr))
+image3 = Image.fromarray(final_img_arr)
+image3 = image3.resize((600,600))
 
-            white_areas = (red == 255) & (blue == 255) & (green == 255)
-            data[..., :-1][white_areas.T] = color
+image2.paste(image3, (50,70), mask=image3)
+image2.paste(x, (470, 490), mask=x)
+image2.paste(circle, (0,0), mask=circle)
 
-            im2 = Image.fromarray(data)
-            circle = im2
+# fonts
 
-            image3 = image1.crop((280,0,1000,720))
-            lum_img = Image.new('L', [720,720] , 0)
-            draw = ImageDraw.Draw(lum_img)
-            draw.pieslice([(0,0), (720,720)], 0, 360, fill = 255, outline = "white")
-            img_arr = np.array(image3)
-            lum_img_arr = np.array(lum_img)
-            final_img_arr = np.dstack((img_arr,lum_img_arr))
-            image3 = Image.fromarray(final_img_arr)
-            image3 = image3.resize((600,600))
+font1 = ImageFont.truetype('assets/font.ttf', 30)
+font2 = ImageFont.truetype('assets/font2.ttf', 70)
+font3 = ImageFont.truetype('assets/font2.ttf', 40)
+font4 = ImageFont.truetype('assets/font2.ttf', 35)
 
-            image2.paste(image3, (50,70), mask=image3)
-            image2.paste(x, (470, 490), mask=x)
-            image2.paste(circle, (0,0), mask=circle)
-            
+image4 = ImageDraw.Draw(image2)
+image4.text((10, 10), MUSIC_BOT_NAME, fill="black", font = font1, align ="left")
+image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
-            # fonts
-            font1 = ImageFont.truetype('assets/font.ttf', 30)
-            font2 = ImageFont.truetype('assets/font2.ttf', 70)
-            font3 = ImageFont.truetype('assets/font2.ttf', 40)
-            font4 = ImageFont.truetype('assets/font2.ttf', 35)
+# title
+title1 = truncate(title)
+image4.text((670, 300), text=title1[0], fill="white", stroke_width=1, stroke_fill="white",font = font3, align ="left") 
+image4.text((670, 350), text=title1[1], fill="white", stroke_width=1, stroke_fill="white", font = font3, align ="left") 
 
-            image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), MUSIC_BOT_NAME, fill="black", font = font1, align ="left")
-            image4.text((670, 150), "NOW PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+ # description
+views = f"Views : {views}"
+duration = f"Duration : {duration} Mins"
+channel = f"Channel : {channel}"
 
-            # title
-            title1 = truncate(title)
-            image4.text((670, 300), text=title1[0], fill="white", stroke_width=1, stroke_fill="white",font = font3, align ="left") 
-            image4.text((670, 350), text=title1[1], fill="white", stroke_width=1, stroke_fill="white", font = font3, align ="left") 
+image4.text((670, 450), text=views, fill="white", font = font4, align ="left") 
+image4.text((670, 500), text=duration, fill="white", font = font4, align ="left") 
+image4.text((670, 550), text=channel, fill="white", font = font4, align ="left")
 
-            # description
-            views = f"Views : {views}"
-            duration = f"Duration : {duration} Mins"
-            channel = f"Channel : {channel}"
+image2 = ImageOps.expand(image2,border=20,fill=make_col())
+image2 = image2.convert('RGB')
 
-            image4.text((670, 450), text=views, fill="white", font = font4, align ="left") 
-            image4.text((670, 500), text=duration, fill="white", font = font4, align ="left") 
-            image4.text((670, 550), text=channel, fill="white", font = font4, align ="left")
-
-            image2 = ImageOps.expand(image2,border=20,fill=make_col())
-            image2 = image2.convert('RGB')
-            try:
-                os.remove(f"cache/thumb{videoid}.png")
-            except:
-                pass
-            image2.save(f"cache/{videoid}_{user_id}.jpg")
-            file = f"cache/{videoid}_{user_id}.jpg"
-            return file
-    except Exception as e:
-        print(e)
-        return YOUTUBE_IMG_URL
+try:
+  os.remove(f"cache/thumb{videoid}.png")
+  except:
+    pass
+    image2.save(f"cache/{videoid}_{user_id}.jpg")
+    file = f"cache/{videoid}_{user_id}.jpg"
+    return file
+except Exception as e:
+print(e)
+return YOUTUBE_IMG_URL
